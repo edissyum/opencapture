4ba7a1acd70bcd413db0a385b0ab17a4166ae2c7
Nathan <nathan.cheval@outlook.fr>
2020-09-16 14:29:05 +0200

    Fix to keep row if one column is empty

--- loadReferencial.py
+++ loadReferencial.py
@@ -44,7 +44,6 @@ if __name__ == '__main__':
     # Insert into database all the supplier not existing into the database
     for taxeNumber in Spreadsheet.referencialSupplierData:
         if not any(str(taxeNumber) in value['vat_number'] for value in listOfExistingSupplier):
-
             args = {
                 'table': 'suppliers',
                 'columns': {
