6117adb752b342dd4f0339feb779084c02e6668e
Nathan <nathan.cheval@outlook.fr>
2020-10-23 13:05:17 +0200

    Workers fixes

--- launch_worker.py
+++ launch_worker.py
@@ -24,13 +24,10 @@ from bin.src.main import launch
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", required=False, help="path to file")
 ap.add_argument("-c", "--config", required=True, help="path to config.xml")
-ap.add_argument("-p", "--path", required=False, help="path to folder containing documents")
 args = vars(ap.parse_args())
 
-if args['path'] is None and args['file'] is None:
-    sys.exit('No file or path were given')
-elif args['path'] is not None and args['file'] is not None:
-    sys.exit('Chose between path or file')
+if args['file'] is None:
+    sys.exit('The file parameters is mandatory')
 
 if not os.path.exists(args['config']):
     sys.exit('Config file couldn\'t be found')
