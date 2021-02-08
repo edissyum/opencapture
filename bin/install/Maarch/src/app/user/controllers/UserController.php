1ecda1855f58e9d33e16c0eafbad5db7ad9addb1
Nathan <nathan.cheval@outlook.fr>
2021-02-08 14:17:57 +0100

    Fix WS

--- bin/install/Maarch/src/app/user/controllers/UserController.php
+++ bin/install/Maarch/src/app/user/controllers/UserController.php
@@ -56,7 +56,7 @@ class UserController
         $aUsers = DatabaseModel::select([
             'select'    => ['id', 'users.user_id', 'firstname', 'lastname', 'status', 'status', 'mail', 'entity_id'],
             'table'     => ['users', 'users_entities', 'usergroup_content'], // NCH01 add usergroup_content
-            'left_join' => ['users.id = users_entities.user_id', 'users.id = usergroup_content.user_id'],
+            'left_join' => ['users.user_id = users_entities.user_id', 'users.id = usergroup_content.user_id'],
             'where'     => ['users.user_id NOT IN (?)', 'status != ?', "entity_id <> ''", 'group_id = ?'],
             'data'      => ['superadmin', 'DEL', $group_id]
         ]);
