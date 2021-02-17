4b3d35253c974c8e6ae49b67f24f074fcdef9c27
Nathan <nathan.cheval@outlook.fr>
2021-02-17 17:37:31 +0100

    WS fix for retrieve GED users

--- bin/install/Maarch/src/app/user/controllers/UserController.php
+++ bin/install/Maarch/src/app/user/controllers/UserController.php
@@ -56,7 +56,7 @@ class UserController
         $aUsers = DatabaseModel::select([
             'select'    => ['users.id', 'users.user_id', 'firstname', 'lastname', 'status', 'status', 'mail', 'entities.entity_id as entity_label' ,'entities.id as entity_id'],
             'table'     => ['users', 'users_entities', 'usergroup_content', 'entities'], // NCH01 add usergroup_content
-            'left_join' => ['users.id = users_entities.user_id', 'users.id = usergroup_content.user_id', 'users_entities.entity_id = entities.entity_id'],
+            'left_join' => ['users.user_id = users_entities.user_id', 'users.id = usergroup_content.user_id', 'users_entities.entity_id = entities.entity_id'],
             'where'     => ['users.user_id NOT IN (?)', 'status != ?', "users_entities.entity_id <> ''", 'group_id = ?'],
             'data'      => ['superadmin', 'DEL', $group_id]
         ]);
