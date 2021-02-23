f67b9a8a183bb00fab499d1287666c87d606585b
Nathan <nathan.cheval@outlook.fr>
2021-02-23 11:08:40 +0100

    add verification when validate form

--- webApp/static/js/videoCodage.js
+++ webApp/static/js/videoCodage.js
@@ -1030,8 +1030,6 @@ $('#validateForm').on('click', function(){
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function (form) {
         if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
             form.classList.add('is-invalid');
         }
     });
