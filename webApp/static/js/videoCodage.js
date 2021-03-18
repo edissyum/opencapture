3fcaaa7db303e36b911db6f0bc4b269f4c3baf1f
Nathan <nathan.cheval@outlook.fr>
2021-03-18 14:51:05 +0100

    OCR On FLy Fix. In some cases it always fill the supplier name

--- webApp/static/js/videoCodage.js
+++ webApp/static/js/videoCodage.js
@@ -177,6 +177,12 @@ function ocrOnFly(isRemoved, inputId, removeWhiteSpace = false, needToBeNumber =
         myImage = zoomImg;
     }
 
+    // Fix to avoid error of value put in the supplier name field
+    if(inputId.id !== 'name'){
+        $('#name').blur()
+        $('#' + inputId.id).focus()
+    }
+
     let input = document.getElementById(inputId.id);
     myImage.imgAreaSelect({
         outerOpacity    : '0',
