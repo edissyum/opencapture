b9d12f44de95600e0f8bb4d631fdb9d154bc99cd
Nathan <nathan.cheval@outlook.fr>
2021-04-06 17:56:14 +0200

    Add missing JS function

--- webApp/static/js/videoCodage.js
+++ webApp/static/js/videoCodage.js
@@ -981,6 +981,33 @@ $('#refuseForm').on('click', function(){
     });
 });
 
+// Validates that the input string is a valid date formatted as "dd/mm/yyyy"
+function isValidDate(dateString)
+{
+    // First check for the pattern
+    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+        return false;
+
+    // Parse the date parts to integers
+    var parts = dateString.split("/");
+    var day = parseInt(parts[0], 10);
+    var month = parseInt(parts[1], 10);
+    var year = parseInt(parts[2], 10);
+
+    // Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        return false;
+
+    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    return day > 0 && day <= monthLength[month - 1];
+};
+
 // Validate form before send it
 $('#validateForm').on('click', function(){
     let form = $('#invoice_info');
