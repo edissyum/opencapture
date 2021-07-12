49dcb6cedfe965fe85faf01242dbbd655821e888
nathan30 <nathan.cheval@outlook.fr>
2021-07-12 14:31:20 +0200

    Various improvments

--- instance/sql/structure.sql
+++ instance/sql/structure.sql
@@ -136,7 +136,7 @@ CREATE TABLE "journals" (
 CREATE TABLE "invoices" (
     "id"                      SERIAL UNIQUE PRIMARY KEY,
     "supplier_id"             INTEGER,
-    'purchage_or'
+    "purchase_or_sale"        VARCHAR(8) DEFAULT 'purchase',
     "vat_number"              VARCHAR DEFAULT NULL,
     "vat_number_position"     VARCHAR(80),
     "invoice_number"          VARCHAR(255),
