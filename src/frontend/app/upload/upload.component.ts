dee521797e167eed0114a17728ee4322d3c55b3b
nathan30 <nathan.cheval@outlook.fr>
2021-07-28 17:45:17 +0200

    Various improvments

--- src/frontend/app/upload/upload.component.ts
+++ src/frontend/app/upload/upload.component.ts
@@ -3,7 +3,7 @@ import {ViewEncapsulation} from '@angular/core';
 import {FormBuilder, FormControl} from "@angular/forms";
 import {FileValidators} from "ngx-file-drag-drop";
 import {API_URL} from "../env";
-import {catchError, tap} from "rxjs/operators";
+import {catchError, finalize, tap} from "rxjs/operators";
 import {of} from "rxjs";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -12,32 +12,7 @@ import {UserService} from "../../services/user.service";
 import {TranslateService} from "@ngx-translate/core";
 import {NotificationService} from "../../services/notifications/notifications.service";
 import {LocalStorageService} from "../../services/local-storage.service";
-import {FlatTreeControl} from "@angular/cdk/tree";
-import {MatTreeFlatDataSource, MatTreeFlattener} from "@angular/material/tree";
 
-interface accountsNode {
-    name: string;
-    id: number;
-    parent_id: any;
-    supplier_id: any;
-    purchase_or_sale: any;
-    number: number,
-    display: boolean,
-    children: any;
-}
-
-interface flatNode {
-    expandable: boolean;
-    name: string;
-    id: number;
-    parent_id: any;
-    supplier_id: any;
-    purchase_or_sale: any;
-    display: boolean;
-    number: number;
-    level: number;
-    children: any;
-}
 
 @Component({
     selector: 'app-upload',
@@ -48,11 +23,11 @@ interface flatNode {
 
 export class UploadComponent implements OnInit {
     headers: HttpHeaders = this.authService.headers;
-    loading         : boolean       = true
-    purchaseOrSale  : string        = 'purchase';
-    customers       : any[]         = [];
-    usersCustomers  : any[]         = [];
-    selectedCustomer: any           = '';
+    selectedCustomer    : any       = '';
+    usersCustomers      : any[]     = [];
+    customers           : any[]     = [];
+    loading             : boolean   = true;
+    purchaseOrSale      : string    = 'purchase';
 
     constructor(
         private http: HttpClient,
@@ -77,33 +52,30 @@ export class UploadComponent implements OnInit {
 
     ngOnInit(): void {
         this.http.get(API_URL + '/ws/accounts/customers/list', {headers: this.authService.headers}).pipe(
-            tap((data: any) => {
-                this.customers = data.customers;
-            }),
+            tap((data: any) => {this.customers = data.customers;}),
             catchError((err: any) => {
                 console.debug(err);
                 this.notify.handleErrors(err);
                 return of(false);
             })
-        ).subscribe()
+        ).subscribe();
 
         this.http.get(API_URL + '/ws/users/getCustomersByUserId/' + this.userService.getUser()['id'], {headers: this.authService.headers}).pipe(
-            tap((data: any) => {
-                this.usersCustomers = data
-            }),
+            tap((data: any) => {this.usersCustomers = data;}),
+            finalize(() => {this.loading = false;}),
             catchError((err: any) => {
                 console.debug(err);
                 this.notify.handleErrors(err);
                 return of(false);
             })
-        ).subscribe()
+        ).subscribe();
 
     }
 
     checkFile(data: any): void {
         if (data && data.length != 0) {
             for (let i = 0; i < data.length; i++) {
-                let file_name = data[i].name
+                let file_name = data[i].name;
                 let file_extension = file_name.split('.').pop();
                 if (file_extension.toLowerCase() != 'pdf') {
                     this.notify.handleErrors(this.translate.instant('UPLOAD.extension_unauthorized', {count: data.length}));
@@ -114,27 +86,30 @@ export class UploadComponent implements OnInit {
     }
 
     hasCustomer(customerId: any) {
-        let user = this.userService.getUser()
-        if (user.privileges == '*'){
-            return true
-        }
-        for (let customer_id of this.usersCustomers) {
-            if(customer_id == customerId) {
-                return true;
-            }
-        }
-        return false
+        let user = this.userService.getUser();
+        if (user.privileges == '*') return true;
+
+        for (let customer_id of this.usersCustomers)
+            if (customer_id == customerId) return true;
+        return false;
     }
 
     setCustomer(customerId: any) {
-        this.selectedCustomer = customerId
+        this.selectedCustomer = customerId;
     }
 
     setPurchaseOrSale(data: any) {
+        this.fileControl = new FormControl(
+            [],
+            [
+                FileValidators.required,
+                FileValidators.fileExtension(['pdf'])
+            ]
+        );
         if (data.tab.textLabel == this.translate.instant('UPLOAD.sale_invoice'))
-            this.purchaseOrSale = 'sale'
+            this.purchaseOrSale = 'sale';
         else
-            this.purchaseOrSale = 'purchase'
+            this.purchaseOrSale = 'purchase';
     }
 
     uploadInvoice(): void {
@@ -153,8 +128,7 @@ export class UploadComponent implements OnInit {
                 return;
             }
         }
-
-        let splitter_or_verifier = this.localeStorageService.get('splitter_or_verifier')
+        let splitter_or_verifier = this.localeStorageService.get('splitter_or_verifier');
         if (splitter_or_verifier !== undefined || splitter_or_verifier !== ''){
             this.http.post(
                 API_URL + '/ws/' + splitter_or_verifier + '/upload?purchaseOrSale=' + this.purchaseOrSale + '&customerId=' + this.selectedCustomer,
