76e1047e2791a09b4af6209b15893459b5974adf
Nathan <nathan.cheval@outlook.fr>
2021-10-11 16:50:22 +0200

    Improve log

--- src/backend/classes/Log.py
+++ src/backend/classes/Log.py
@@ -15,8 +15,22 @@
 
 # @dev : Nathan Cheval <nathan.cheval@outlook.fr>
 
+import os
 import logging
 from logging.handlers import RotatingFileHandler
+from inspect import getframeinfo, stack
+
+
+def caller_reader(f):
+    """This wrapper updates the context with the callor infos"""
+
+    def wrapper(self, *args):
+        caller = getframeinfo(stack()[1][0])
+        self._filter.file = os.path.basename(caller.filename)
+        self._filter.line_n = caller.lineno
+        return f(self, *args)
+
+    return wrapper
 
 
 class Log:
@@ -26,19 +40,36 @@ class Log:
         self.LOGGER = logging.getLogger('Open-Capture')
         if self.LOGGER.hasHandlers():
             self.LOGGER.handlers.clear()  # Clear the handlers to avoid double logs
-
         log_file = RotatingFileHandler(path, mode='a', maxBytes=5 * 1024 * 1024,
                                        backupCount=2, encoding=None, delay=False)
-        formatter = logging.Formatter('[%(name)-14s] %(asctime)s %(levelname)s %(message)s',
-                                      datefmt='%d-%m-%Y %H:%M:%S')
+        formatter = logging.Formatter(
+            '[%(name)-17s] [%(file)-22sline %(line_n)-3s] %(asctime)s %(levelname)s %(message)s',
+            datefmt='%d-%m-%Y %H:%M:%S')
         log_file.setFormatter(formatter)
         self.LOGGER.addHandler(log_file)
+
+        self.LOGGER.filters.clear()
+        self._filter = CallerFilter()
+        self.LOGGER.addFilter(self._filter)
         self.LOGGER.setLevel(logging.DEBUG)
 
+    @caller_reader
     def info(self, msg):
         self.LOGGER.info(msg)
 
+    @caller_reader
     def error(self, msg, send_notif=True):
         if self.smtp.enabled and send_notif:
             self.smtp.send_notification(msg, self.filename)
         self.LOGGER.error(msg)
+
+
+class CallerFilter(logging.Filter):
+    """ This class adds some context to the log record instance """
+    file = ''
+    line_n = ''
+
+    def filter(self, record):
+        record.file = self.file
+        record.line_n = self.line_n
+        return True
