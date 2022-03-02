1edc37bba1688bd428c80f79d3b1180a9067fd08
nathan30 <nathan.cheval@outlook.fr>
2022-03-02 16:03:07 +0100

    Refactor

--- src/frontend/services/pipes/filter.pipe.ts
+++ src/frontend/services/pipes/filter.pipe.ts
@@ -7,9 +7,6 @@ import {PipeTransform, Pipe, Injectable} from '@angular/core';
 export class FilterPipe implements PipeTransform {
     transform(items: any[], field : string, value : string): any[] {
         if (!items) return [];
-        console.log(items);
-        console.log(field);
-        console.log(value);
         if (!value || value.length === 0) return items;
         return items.filter(it =>
         it[field].toLowerCase().indexOf(value.toLowerCase()) !== -1);
