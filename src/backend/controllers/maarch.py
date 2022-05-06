7cce4b5594a5be3a608508da4e24a3871ef68a06
Nathan <nathan.cheval@outlook.fr>
2022-05-06 11:47:40 +0200

    Typo fix

--- src/backend/controllers/maarch.py
+++ src/backend/controllers/maarch.py
@@ -21,102 +21,102 @@ from src.backend.import_classes import _MaarchWebServices
 
 def test_connection(args):
     _vars = create_classes_from_current_config()
-    ws = _MaarchWebServices(
+    _ws = _MaarchWebServices(
         args['host'],
         args['login'],
         args['password'],
         _vars[5],
         _vars[1]
     )
-    return ws.status
+    return _ws.status
 
 
 def get_users(args):
     _vars = create_classes_from_current_config()
-    ws = _MaarchWebServices(
+    _ws = _MaarchWebServices(
         args['host'],
         args['login'],
         args['password'],
         _vars[5],
         _vars[1]
     )
-    users = ws.retrieve_users()
+    users = _ws.retrieve_users()
     return users
 
 
 def get_doctypes(args):
     _vars = create_classes_from_current_config()
-    ws = _MaarchWebServices(
+    _ws = _MaarchWebServices(
         args['host'],
         args['login'],
         args['password'],
         _vars[5],
         _vars[1]
     )
-    doctypes = ws.retrieve_doctypes()
+    doctypes = _ws.retrieve_doctypes()
     return doctypes
 
 
 def get_entities(args):
     _vars = create_classes_from_current_config()
-    ws = _MaarchWebServices(
+    _ws = _MaarchWebServices(
         args['host'],
         args['login'],
         args['password'],
         _vars[5],
         _vars[1]
     )
-    entities = ws.retrieve_entities()
+    entities = _ws.retrieve_entities()
     return entities
 
 
 def get_custom_fields(args):
     _vars = create_classes_from_current_config()
-    ws = _MaarchWebServices(
+    _ws = _MaarchWebServices(
         args['host'],
         args['login'],
         args['password'],
         _vars[5],
         _vars[1]
     )
-    custom_fields = ws.retrieve_custom_fields()
+    custom_fields = _ws.retrieve_custom_fields()
     return custom_fields
 
 
 def get_priorities(args):
     _vars = create_classes_from_current_config()
-    ws = _MaarchWebServices(
+    _ws = _MaarchWebServices(
         args['host'],
         args['login'],
         args['password'],
         _vars[5],
         _vars[1]
     )
-    entities = ws.retrieve_priorities()
+    entities = _ws.retrieve_priorities()
     return entities
 
 
 def get_statuses(args):
     _vars = create_classes_from_current_config()
-    ws = _MaarchWebServices(
+    _ws = _MaarchWebServices(
         args['host'],
         args['login'],
         args['password'],
         _vars[5],
         _vars[1]
     )
-    statuses = ws.retrieve_statuses()
+    statuses = _ws.retrieve_statuses()
     return statuses
 
 
 def get_indexing_models(args):
     _vars = create_classes_from_current_config()
-    ws = _MaarchWebServices(
+    _ws = _MaarchWebServices(
         args['host'],
         args['login'],
         args['password'],
         _vars[5],
         _vars[1]
     )
-    indexing_models = ws.retrieve_indexing_models()
+    indexing_models = _ws.retrieve_indexing_models()
     return indexing_models
