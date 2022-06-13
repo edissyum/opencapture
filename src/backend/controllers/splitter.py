703564ac9f07efc69101b0c4bb14d6a6b4a10725
Brich40 <brich.oussama@gmail.com>
2022-06-13 15:21:00 +0200

    Fix status after validate

--- src/backend/controllers/splitter.py
+++ src/backend/controllers/splitter.py
@@ -611,7 +611,7 @@ def validate(args):
                 """
                 splitter.change_status({
                     'id': args['batchMetadata']['id'],
-                    'status': 'NEW'
+                    'status': 'END'
                 })
 
     return {"OK": True}, 200
