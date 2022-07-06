d64400d8973ed24263dc41c0b82842dd018b77d5
nathan30 <nathan.cheval@outlook.fr>
2022-07-06 15:28:33 +0200

    Improve supplier detection with email

--- src/backend/process/FindSupplier.py
+++ src/backend/process/FindSupplier.py
@@ -112,6 +112,11 @@ class FindSupplier:
                     if supplier:
                         return supplier, line
 
+                for _data in re.finditer(r"" + regex + "", corrected_line.replace(' ', '').replace(',', '').replace('(', '').replace(')', '')):
+                    supplier = self.search_suplier(column, _data.group().replace(' ', ''))
+                    if supplier:
+                        return supplier, line
+
             for _data in re.finditer(r"" + regex + "", corrected_line.replace('.', '').replace(',', '').replace('(', '').replace(')', '').replace('-', '')):
                 supplier = self.search_suplier(column, _data.group().replace(' ', ''))
                 if supplier:
