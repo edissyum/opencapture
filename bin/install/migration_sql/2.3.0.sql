b4ece1fa3c6709e70bf7f74ad440f3523a2b8de4
brich.oussama <brich.oussama@gmail.com>
2022-07-08 14:52:16 +0200

    Fix migration queries

--- bin/install/migration_sql/2.3.0.sql
+++ bin/install/migration_sql/2.3.0.sql
@@ -92,7 +92,7 @@ UPDATE outputs_types SET "data" = '{
       },
       {
         "id": "add_to_zip",
-        "hint": "Ajouter le fichier au ZIP, [Except=doctype1] mentionne les type de document à ne pas ajouter dans le ZIP",
+        "hint": "Ajouter le fichier au ZIP, [Except=doctype1] mentionne les types de documents à ne pas ajouter dans le ZIP",
         "type": "text",
         "label": "Nom du fichier ZIP à exporter",
         "required": "false",
