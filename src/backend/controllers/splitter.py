97f9be18409460a3a5b554b03791980cbdddc9b9
brich.oussama <brich.oussama@gmail.com>
2022-07-08 11:54:04 +0200

    Change close status

--- src/backend/controllers/splitter.py
+++ src/backend/controllers/splitter.py
@@ -648,7 +648,7 @@ def validate(args):
                 """
                 splitter.change_status({
                     'id': args['batchMetadata']['id'],
-                    'status': 'NEW'
+                    'status': 'END'
                 })
 
     return {"OK": True}, 200
