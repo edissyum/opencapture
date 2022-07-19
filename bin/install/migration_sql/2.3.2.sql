9a67297c21d977aa4b4c72a59c7fab17b95f6e8c
Nathan Cheval <nathan.cheval@outlook.fr>
2022-07-19 15:23:02 +0200

    Update 2.3.2.sql

--- bin/install/migration_sql/2.3.2.sql
+++ bin/install/migration_sql/2.3.2.sql
@@ -1,2 +1,5 @@
 -- Improve positions masks
 ALTER TABLE positions_masks ADD COLUMN form_id INTEGER;
+
+-- Improve REGEX
+UPDATE regex SET content = '(VO(TRE|S)|V\.\/)?\s*(NUM(E|É)RO|N(O|°|º|R.)?|R(E|É)F(\.)?((E|É)RENCE)?)\s*(DE)?\s*((COMMANDE|COM(\.)|CDE|DOCUMENT\s*EXTERNE)\s*(INTERNET|WEB)?)|((COMMANDE|CMDE|CDE)\s*(NUM(E|É)RO|N(O|°|º|R.)))\s*(CLIENT)?\s*:?.*' WHERE lang = 'fra' AND regex_id = 'orderNumberRegex';
