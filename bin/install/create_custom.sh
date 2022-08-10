8abfa2127e91531ceae094b4ec4bef4a3fcd22cb
nathan30 <nathan.cheval@outlook.fr>
2022-08-10 15:32:10 +0200

    Typo fix

--- bin/install/create_custom.sh
+++ bin/install/create_custom.sh
@@ -55,7 +55,7 @@ if [ -z "$customId" ]; then
     exit 2
 fi
 
-if [ "$customId" == 'custom' ] ; then
+if [ "$customId" == 'custom' ]; then
     echo "##########################################################################"
     echo "              Please do not create a custom called 'custom'"
     echo "      Exemple of command line call : sudo ./update.sh -c edissyum_bis"
