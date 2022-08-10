346a6153ed67678438f4d917a0a50cc7bd57a07b
nathan30 <nathan.cheval@outlook.fr>
2022-08-10 10:53:09 +0200

    Bump version 2.4.0

--- bin/install/install.sh
+++ bin/install/install.sh
@@ -299,10 +299,6 @@ export PGPASSWORD=$databasePassword && psql -U"$databaseUsername" -h"$hostname" 
 export PGPASSWORD=$databasePassword && psql -U"$databaseUsername" -h"$hostname" -p"$port" -c "UPDATE inputs SET input_folder=REPLACE(input_folder, '/var/share/' , '/var/share/$customId/')" "$databaseName" > /dev/null
 export PGPASSWORD=$databasePassword && psql -U"$databaseUsername" -h"$hostname" -p"$port" -c "UPDATE outputs SET data = jsonb_set(data, '{options,parameters, 0, value}', '\"/var/share/$customId/export/\"') WHERE data #>>'{options,parameters, 0, id}' = 'folder_out';" "$databaseName" > /dev/null
 
-echo ""
-echo "#################################################################################################"
-echo ""
-
 ####################
 # Create the Apache service for backend
 touch /etc/apache2/sites-available/opencapture.conf
