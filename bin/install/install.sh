05b130574e4c166e5df3a0f22809597103a7b1ff
nathan30 <nathan.cheval@outlook.fr>
2022-08-31 11:24:50 +0200

    Improve install.sh for using with python venv

--- bin/install/install.sh
+++ bin/install/install.sh
@@ -339,11 +339,18 @@ export PGPASSWORD=$databasePassword && psql -U"$databaseUsername" -h"$hostname" 
 ####################
 # Create the Apache service for backend
 touch /etc/apache2/sites-available/opencapture.conf
+wsgiDaemonProcessLine="WSGIDaemonProcess opencapture user=$user group=$group home=$defaultPath threads=$nbThreads processes=$nbProcesses"
+if [ $pythonVenv = 'true' ]; then
+    sitePackageLocation=$(python3 -c 'import site; print(site.getsitepackages()[0])')
+    if [ -z $sitePackageLocation ] then
+        wsgiDaemonProcessLine="WSGIDaemonProcess opencapture user=$user group=$group home=$defaultPath threads=$nbThreads processes=$nbProcesses python-path=$sitePackageLocation"
+    fi
+fi
 su -c "cat > /etc/apache2/sites-available/opencapture.conf << EOF
 <VirtualHost *:80>
     ServerName localhost
     DocumentRoot $defaultPath
-    WSGIDaemonProcess opencapture user=$user group=$group home=$defaultPath threads=$nbThreads processes=$nbProcesses
+    $wsgiDaemonProcessLine
     WSGIScriptAlias /backend_oc /var/www/html/opencaptureforinvoices/wsgi.py
 
     <Directory $defaultPath>
