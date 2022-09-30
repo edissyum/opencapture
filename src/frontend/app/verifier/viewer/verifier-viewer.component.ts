43973f0fb9ba5f232b34ef4e517a50fc4e32da2b
Nathan <nathan.cheval@outlook.fr>
2022-09-30 16:04:13 +0200

    Improve pattern to handle negative value

--- src/frontend/app/verifier/viewer/verifier-viewer.component.ts
+++ src/frontend/app/verifier/viewer/verifier-viewer.component.ts
@@ -102,11 +102,11 @@ export class VerifierViewerComponent implements OnInit {
         other         : []
     };
     pattern                 : any         = {
-        alphanum                        : '^[0-9a-zA-Z\\s\']*$',
-        alphanum_extended               : '^[0-9a-zA-Z-/#,\\.\'\\s]*$',
-        alphanum_extended_with_accent   : '^[0-9a-zA-Z\\u00C0-\\u017F-/#,\'\\.\\s]*$',
-        number_int                      : '^[0-9]*$',
-        number_float                    : '^[0-9]*([.][0-9]*)*$',
+        alphanum                        : '^[(\\-)?0-9a-zA-Z\\s\']*$',
+        alphanum_extended               : '^[(\\-)?0-9a-zA-Z-/#,\\.\'\\s]*$',
+        alphanum_extended_with_accent   : '^[(\\-)?0-9a-zA-Z\\u00C0-\\u017F-/#,\'\\.\\s]*$',
+        number_int                      : '^[(\\-)?0-9]*$',
+        number_float                    : '^[(\\-)?0-9]*([.][0-9]*)*$',
         char                            : '^[A-Za-z\\s]*$',
         email                           : '^[a-zA-Z0-9_\\.\\+-]+@[a-zA-Z0-9-]+\\.(fr|com|org|eu|law)+$'
     };
