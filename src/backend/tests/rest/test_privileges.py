06a13676b90dd7b1521d5ef11c404c32e980a1f9
Nathan Cheval <nathan.cheval@outlook.fr>
2022-11-29 16:59:56 +0100

    Add privileges tests

---
+++ src/backend/tests/rest/test_privileges.py
@@ -0,0 +1,45 @@
+# This file is part of Open-Capture.
+
+# Open-Capture is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Open-Capture is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Open-Capture. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+
+# @dev : Nathan Cheval <nathan.cheval@outlook.fr>
+
+import unittest
+import warnings
+from src.backend import app
+from src.backend.tests import get_db, get_token
+
+
+class PrivilegesTest(unittest.TestCase):
+    def setUp(self):
+        self.db = get_db()
+        self.app = app.test_client()
+        self.token = get_token('admin')
+        warnings.filterwarnings('ignore', message="unclosed", category=ResourceWarning)
+
+    def test_successful_get_privileges_list(self):
+        response = self.app.get('/test/ws/privileges/list',
+                                headers={"Content-Type": "application/json", 'Authorization': 'Bearer ' + self.token})
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(dict, type(response.json))
+        self.assertTrue('privileges' in response.json)
+        self.assertTrue('parent' in response.json)
+
+    def test_successful_get_privileges_by_role(self):
+        response = self.app.get('/test/ws/privileges/getbyRoleId/1',
+                                headers={"Content-Type": "application/json", 'Authorization': 'Bearer ' + self.token})
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(str, type(response.json))
+        self.assertEqual('*', response.json)
+
