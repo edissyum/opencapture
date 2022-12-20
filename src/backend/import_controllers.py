1e8406787389038fa2a5f13d61a49f9da2fd936f
brich.oussama <brich.oussama@gmail.com>
2022-12-20 16:33:49 +0100

    Fix AI import

--- src/backend/import_controllers.py
+++ src/backend/import_controllers.py
@@ -117,4 +117,4 @@ elif 'controllers' in custom_array['tasks_watcher']['path']:
 if 'artificial_intelligence' or 'controllers' not in custom_array['artificial_intelligence']['path']:
     from .controllers import artificial_intelligence
 elif 'controllers' in custom_array['artificial_intelligence']['path']:
-    inputs = getattr(__import__(custom_array['artificial_intelligence']['path'], fromlist=[custom_array['artificial_intelligence']['module']]), custom_array['artificial_intelligence']['module'])
+    artificial_intelligence = getattr(__import__(custom_array['artificial_intelligence']['path'], fromlist=[custom_array['artificial_intelligence']['module']]), custom_array['artificial_intelligence']['module'])
