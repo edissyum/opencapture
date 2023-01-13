9b9f2eb4912e26d0270522d947ab5e0d29deecec
Brich Oussama <oussama.brich@alumni.univ-avignon.fr>
2023-01-13 16:08:45 +0100

    Add separator generate unittest

--- src/backend/tests/rest/test_doctype.py
+++ src/backend/tests/rest/test_doctype.py
@@ -17,6 +17,9 @@
 
 import unittest
 import warnings
+import binascii
+import base64
+
 from src.backend import app
 from src.backend.tests import CUSTOM_ID, get_db, get_token
 
@@ -79,10 +82,6 @@ class DoctypeTest(unittest.TestCase):
         doctype = self.create_folder()
         self.assertEqual(200, doctype.status_code)
 
-    def test_successful_update_doctype(self):
-        res = self.update_doctype()
-        self.assertEqual(200, res.status_code)
-
     def test_successful_get_doctypes_list(self):
         self.create_doctype()
         response = self.app.get(f'/{CUSTOM_ID}/ws/doctypes/list/1',
@@ -90,6 +89,31 @@ class DoctypeTest(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         self.assertEqual(dict, type(response.json))
         self.assertEqual(len(response.json['doctypes']), 1)
+
+    def test_generate_doctype_separator(self):
+        payload = {
+            "type": "docTypeSeparator",
+            "key": "test_doctype",
+            "label": "Test doctype updated"
+        }
+
+        response = self.app.post(f'/{CUSTOM_ID}/ws/doctypes/generateSeparator',
+                                 json=payload,
+                                 headers={"Content-Type": "application/json", 'Authorization': 'Bearer ' + self.token})
+
+        try:
+            base64.b64decode(response.json['encoded_file'])
+            base64.b64decode(response.json['encoded_thumbnail'])
+            is_base64 = True
+        except binascii.Error:
+            is_base64 = False
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(True, is_base64)
+
+    def test_successful_update_doctype(self):
+        response = self.update_doctype()
+        self.assertEqual(200, response.status_code)
 
     def tearDown(self) -> None:
         self.db.execute("DELETE FROM doctypes WHERE key IN ('test_folder', 'test_doctype')")
