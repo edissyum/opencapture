7593bcdbbc4df39cbd25969e223a138fa2e97b7c
Brich Oussama <brich.oussama@gmail.com>
2023-02-10 11:04:51 +0100

    Add default metadata to pdf file

--- src/backend/classes/Files.py
+++ src/backend/classes/Files.py
@@ -526,6 +526,8 @@ class Files:
                         pdf_page.rotate(page['rotation'])
                     pdf_writer.add_page(pdf_page)
 
+                pdf_writer.add_metadata(pdf_reader.metadata)
+
                 title = ''
                 if 'title' in args['documents'][index]['metadata']:
                     title = args['documents'][index]['metadata']['title']
