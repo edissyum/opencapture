7abceb4ab59c0ff72e263bba1d1dd7fb0ae27dfd
Brich Oussama <brich.oussama@gmail.com>
2023-02-20 17:57:13 +0100

    Update unittests

--- src/backend/tests/rest/test_splitter.py
+++ src/backend/tests/rest/test_splitter.py
@@ -16,6 +16,8 @@
 # @dev : Brich Oussama <oussama.brich@edissyum.com>
 
 import shutil
+from time import sleep
+
 import urllib3
 import unittest
 import warnings
@@ -46,7 +48,6 @@ class SplitterTest(unittest.TestCase):
 
         pdf_url = 'https://open-capture.com/wp-content/uploads/2022/11/splitter_test.pdf'
         pdf_path = './instance/upload/splitter/splitter_test.pdf'
-
         http = urllib3.PoolManager()
         with http.request('GET', pdf_url, preload_content=False) as r, open(pdf_path, 'wb') as out_file:
             shutil.copyfileobj(r, out_file)
@@ -66,7 +67,14 @@ class SplitterTest(unittest.TestCase):
 
     def test_successful_get_batches_list(self):
         self.create_batch()
-        response = self.app.get(f'/{CUSTOM_ID}/ws/splitter/batches/user/1/paging/0/16/today/NEW',
+        payload = {
+            'page': 0,
+            'size': 10,
+            'userId': 1,
+            'status': 'NEW',
+            'today': 'today'
+        }
+        response = self.app.post(f'/{CUSTOM_ID}/ws/splitter/batches/list', json=payload,
                                 headers={"Content-Type": "application/json", 'Authorization': 'Bearer ' + self.token})
         self.assertEqual(200, response.status_code)
         self.assertEqual(True, len(response.json['batches']) > 0)
@@ -75,9 +83,13 @@ class SplitterTest(unittest.TestCase):
         self.create_batch()
         self.db.execute("SELECT * FROM splitter_batches")
         batches = self.db.fetchall()
-        response = self.app.get(f"/{CUSTOM_ID}/ws/splitter/documents/{batches[0]['id']}",
+        payload = {
+            'userId': 1,
+            'batchId': batches[0]['id'],
+        }
+        response = self.app.post(f"/{CUSTOM_ID}/ws/splitter/batches/list",
                                 headers={"Content-Type": "application/json",
-                                         'Authorization': 'Bearer ' + self.token})
+                                         'Authorization': 'Bearer ' + self.token}, json=payload)
         self.assertEqual(200, response.status_code)
         self.assertEqual(2, len(response.json['documents']))
 
