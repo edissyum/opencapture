355a3469b5107a94ff43511eaf26950a3c0543d7
nathan30 <nathan.cheval@outlook.fr>
2023-04-12 09:42:32 +0200

    Fix migration_sql script

--- bin/install/migration_sql/3.0.0.sql
+++ bin/install/migration_sql/3.0.0.sql
@@ -3,7 +3,7 @@ ALTER TABLE users ADD COLUMN "mode" VARCHAR(10) DEFAULT 'standard';
 ALTER TABLE roles ALTER COLUMN "label" SET DATA TYPE VARCHAR(255);
 
 WITH new_role_id as (
-    INSERT INTO roles ("label_short", "label", "editable") VALUES ('user_wsa', 'Utilisateur WebServices', 'true') returning id
+    INSERT INTO roles ("label_short", "label", "editable") VALUES ('user_ws', 'Utilisateur WebServices', 'true') returning id
 )
 INSERT INTO roles_privileges ("role_id", "privileges_id") VALUES ((SELECT id from new_role_id), '{"data" : "[3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 47, 48, 49, 50, 51, 52, 54, 55]"}');
 
