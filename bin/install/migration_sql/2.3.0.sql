f799cb9ec307f9c4788957cdaeb5cb1bdf80fba3
Brich Oussama <brich.oussama@gmail.com>
2023-05-29 11:54:25 +0200

    Undo old migration edit

--- bin/install/migration_sql/2.3.0.sql
+++ bin/install/migration_sql/2.3.0.sql
@@ -118,7 +118,7 @@ UPDATE outputs_types SET "data" = '{
         "placeholder": "pdf"
       },
       {
-        "id": "zip_filename",
+        "id": "add_to_zip",
         "hint": "Ajouter le fichier au ZIP, [Except=doctype1] mentionne les types de documents à ne pas ajouter dans le ZIP",
         "type": "text",
         "label": "Nom du fichier ZIP à exporter",
