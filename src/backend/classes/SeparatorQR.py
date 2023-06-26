1b90b7f0ab27ad193077528eedf6ed1b6cbfc51c
Brich Oussama <brich.oussama@gmail.com>
2023-06-26 10:21:22 +0200

    Undo to default function format

--- src/backend/classes/SeparatorQR.py
+++ src/backend/classes/SeparatorQR.py
@@ -179,6 +179,7 @@ class SeparatorQR:
     def parse_xml_multi(self):
         if self.barcodes is None:
             return
+
         for barcode in self.barcodes:
             self.pages.append({
                 "qr_code": barcode['text'],
