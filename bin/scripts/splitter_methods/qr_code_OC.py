6de8680e6b0b8c6a64015789048c64ad02fbac58
Brich Oussama <brich.oussama@gmail.com>
2023-07-11 16:04:18 +0200

    Use full image instead if thumbs

--- bin/scripts/splitter_methods/qr_code_OC.py
+++ bin/scripts/splitter_methods/qr_code_OC.py
@@ -42,7 +42,7 @@ def process(args):
     saved_pages = args['files'].save_img_with_pdf2image(args['file'], batch_folder_path + "page")
     args['files'].save_img_with_pdf2image_min(args['file'], batch_thumbs_path + "page", single_file=False, module='splitter')
 
-    list_files = args['files'].sorted_file(batch_thumbs_path, 'jpg')
+    list_files = args['files'].sorted_file(batch_folder_path, 'jpg')
     blank_pages = []
 
     # Remove blank pages
