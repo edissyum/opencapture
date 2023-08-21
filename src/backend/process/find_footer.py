1f1bc458b96b66ede89ccaf3db18eb63641f5566
Nathan Cheval <nathan.cheval@outlook.fr>
2023-08-21 16:20:17 +0200

    Improve find footer process

--- src/backend/process/find_footer.py
+++ src/backend/process/find_footer.py
@@ -363,15 +363,15 @@ class FindFooter:
             vat_rate = self.return_max(self.vat_rate)
             vat_amount = self.return_max(self.vat_amount)
 
-            if total_ht is False and total_ttc and vat_rate:
+            if total_ht is False and (total_ttc and total_ttc[0]) and (vat_rate and vat_rate[0]):
                 total_ht = [float("%.2f" % (float(total_ttc[0]) / (1 + float(vat_rate[0] / 100)))), (('', ''), ('', '')), True]
-            if total_ht is False and total_ttc and vat_amount:
+            if total_ht is False and (total_ttc and total_ttc[0]) and (vat_amount and vat_amount[0]):
                 total_ht = [float("%.2f" % (float(total_ttc[0]) - float(vat_amount[0]))), (('', ''), ('', '')), True]
             elif (total_ttc is False or not total_ttc[0]) and total_ht and total_ht[0] and vat_rate and vat_rate[0]:
                 total_ttc = [float("%.2f" % (float(total_ht[0]) + (float(total_ht[0]) * float(float(vat_rate[0]) / 100)))), (('', ''), ('', '')), True]
             elif (total_ttc is False or not total_ttc[0]) and total_ht and total_ht[0] and vat_amount and vat_amount[0]:
                 total_ttc = [float("%.2f" % (float(total_ht[0]) + float(vat_amount[0]))), (('', ''), ('', '')), True]
-            elif vat_rate is False and total_ht and total_ttc:
+            elif vat_rate is False and (total_ht and total_ht[0]) and (total_ttc and total_ttc[0]):
                 if vat_amount is False:
                     vat_amount = [float("%.2f" % (float(total_ttc[0]) - float(total_ht[0]))), (('', ''), ('', '')), True]
                 vat_rate = [float("%.2f" % (float(vat_amount[0]) / float(total_ht[0]) * 100)), (('', ''), ('', '')), True]
