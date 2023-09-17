7062be8e07a46da39c663c61ef8b770f9468c20c
Brich Oussama <brich.oussama@gmail.com>
2023-09-17 15:43:06 +0200

    Change forms list privileges

--- src/backend/rest/forms.py
+++ src/backend/rest/forms.py
@@ -27,7 +27,7 @@ bp = Blueprint('forms', __name__, url_prefix='/ws/')
 @auth.token_required
 def get_forms(module):
     if 'skip' not in request.environ or not request.environ['skip']:
-        list_priv = ['settings', 'forms_list'] if module == 'verifier' else []
+        list_priv = ['settings', 'forms_list'] if module == 'verifier' else ['access_splitter | forms_list_splitter']
         if not privileges.has_privileges(request.environ['user_id'], list_priv):
             return jsonify({'errors': gettext('UNAUTHORIZED_ROUTE'), 'message': f'/forms/{module}/list'}), 403
 
