e5fc9f2a6d1010a7a10ea9a71353d5322d3c1ac0
nathan30 <nathan.cheval@outlook.fr>
2023-10-18 10:11:56 +0200

    Finalize config rest validators

--- src/frontend/app/settings/general/docservers/docservers.component.ts
+++ src/frontend/app/settings/general/docservers/docservers.component.ts
@@ -112,13 +112,13 @@ export class DocserversComponent implements OnInit {
         ).subscribe();
     }
 
-    updateValue(event: any, id: number, docserver: string) {
+    updateValue(event: any, id: number) {
         this.updateLoading = true;
         const value = event.target.value;
         this.docservers.forEach((element: any) => {
             if (element.id === id) {
                 element.path = value;
-                this.http.put(environment['url'] + '/ws/config/updateDocserver/' + element.id, {'data': element}, {headers: this.authService.headers}).pipe(
+                this.http.put(environment['url'] + '/ws/config/updateDocserver/' + element.id, {'args': element}, {headers: this.authService.headers}).pipe(
                     tap(() => {
                         element.updateMode = false;
                         this.updateLoading = false;
