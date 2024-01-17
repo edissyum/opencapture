ec153e603e2576e600c424f6d6c2a39a68cc5c7d
Brich Oussama <brich.oussama@gmail.com>
2024-01-17 11:40:19 +0100

    Fix out of range for find_name process

--- src/backend/process/find_name.py
+++ src/backend/process/find_name.py
@@ -237,12 +237,12 @@ class FindName:
                                             if fuzz.ratio(splitted_line[cpt + 1].lower(), name.lower()) >= 80:
                                                 firstname = splitted_line[cpt + 1].title()
                                                 lastname = splitted_line[cpt + 2].title()
-                                                if lastname.lower() in ['de', 'el', 'da', 'le'] and len(splitted_line) >= cpt + 3:
+                                                if lastname.lower() in ['de', 'el', 'da', 'le'] and len(splitted_line) > cpt + 3:
                                                     lastname += ' ' + splitted_line[cpt + 3].title()
                                             elif fuzz.ratio(splitted_line[cpt + 2].lower(), name.lower()) >= 80 or \
                                                     (len(splitted_line) > cpt + 3  and splitted_line[cpt + 3].lower()):
                                                 lastname = splitted_line[cpt + 1].title()
-                                                if lastname.lower() in ['de', 'el', 'da', 'le'] and len(splitted_line) >= cpt + 3:
+                                                if lastname.lower() in ['de', 'el', 'da', 'le'] and len(splitted_line) > cpt + 3:
                                                     lastname += ' ' + splitted_line[cpt + 2].title()
                                                     firstname = splitted_line[cpt + 3].title()
                                                 else:
