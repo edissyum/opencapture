ecd642d160e8aece3e0257ddcb02226c3c454412
Nathan <nathan.cheval@outlook.fr>
2024-04-19 12:00:18 +0200

    Various code improvments / refactor

--- src/backend/process/find_custom.py
+++ src/backend/process/find_custom.py
@@ -306,7 +306,7 @@ class FindCustom:
                         data = self.check_format(data, regex_settings)
                         if data:
                             if regex_settings['format'] == 'amount':
-                                data = re.sub(r",", '.', data)
+                                data = data.replace(',', '.')
 
                             if 'remove_spaces' in regex_settings and regex_settings['remove_spaces']:
                                 data = re.sub(r"\s*", '', data)
