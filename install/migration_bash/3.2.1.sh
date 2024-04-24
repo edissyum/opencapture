fbaa30371241e420094d9da2d7532864cb283d7b
nathan30 <nathan.cheval@outlook.fr>
2024-04-24 14:10:12 +0200

    Update default referencial JSON file

--- install/migration_bash/3.2.1.sh
+++ install/migration_bash/3.2.1.sh
@@ -17,8 +17,8 @@
 # @dev : Nathan Cheval <nathan.cheval@outlook.fr>
 
 if [ "$EUID" -ne 0 ]; then
-Echo "$(basename "$0") needed to be launch by user with root privileges"
-Exit 1
+    echo "$(basename "$0") needed to be launch by user with root privileges"
+    exit 1
 fi
 
 apache2Path="/etc/apache2/sites-available/"
@@ -30,6 +30,20 @@ sed -i "s|<Directory|Header always set Strict-Transport-Security \"max-age=31536
 #####################
 # Add ErrorDocument to apache2 configuration
 sed -i "s|<VirtualHost|ErrorDocument 400 /src/assets/error_pages/400.html\nErrorDocument 401 /src/assets/error_pages/401.html\nErrorDocument 403 /src/assets/error_pages/403.html\nErrorDocument 404 /src/assets/error_pages/404.html\nErrorDocument 500 /src/assets/error_pages/500.html\nErrorDocument 501 /src/assets/error_pages/501.html\nErrorDocument 502 /src/assets/error_pages/502.html\nErrorDocument 503 /src/assets/error_pages/503.html\nErrorDocument 504 /src/assets/error_pages/504.html\n\n<VirtualHost|" $apache2Path/opencapture.conf
+
+#####################
+# Update default referencial JSON file
+opencapturePath="/var/www/html/opencapture/"
+cd $opencapturePath
+
+SECTIONS=$(crudini --get $opencapturePath/custom/custom.ini | sed 's/:.*//')
+for custom_name in ${SECTIONS[@]}; do
+    custom_name=${custom_name//[\.\-]/_}
+    custom_name=$(echo "$custom_name" | tr "[:upper:]" "[:lower:]")
+    cp $opencapturePath/instance/referencial/default_referencial_supplier_index.json.default $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier_index.json 2>/dev/null
+    rm -f $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier.ods
+    touch $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier.csv
+done
 
 #####################
 # Restart apache2
