10a4edba89d3b1d0dddf3906310601631ecade76
Nathan Cheval <nathan.cheval@outlook.fr>
2024-08-16 14:17:50 +0200

    Improve migrations scripts

--- install/migration_bash/3.3.0.sh
+++ install/migration_bash/3.3.0.sh
@@ -25,24 +25,48 @@ apache2Path="/etc/apache2/sites-available/"
 
 #####################
 # Add HSTS and X-Content-Type to apache2 configuration
-sed -i "s|<Directory|Header always set Strict-Transport-Security \"max-age=31536000; includeSubDomains; preload\"\n    Header always set X-Content-Type-Options: nosniff\n\n    <Directory|" $apache2Path/opencapture.conf
+#sed -i "s|<Directory|Header always set Strict-Transport-Security \"max-age=31536000; includeSubDomains; preload\"\n    Header always set X-Content-Type-Options: nosniff\n\n    <Directory|" $apache2Path/opencapture.conf
 
 #####################
 # Add ErrorDocument to apache2 configuration
-sed -i "s|<VirtualHost|ErrorDocument 400 /src/assets/error_pages/400.html\nErrorDocument 401 /src/assets/error_pages/401.html\nErrorDocument 403 /src/assets/error_pages/403.html\nErrorDocument 404 /src/assets/error_pages/404.html\nErrorDocument 500 /src/assets/error_pages/500.html\nErrorDocument 501 /src/assets/error_pages/501.html\nErrorDocument 502 /src/assets/error_pages/502.html\nErrorDocument 503 /src/assets/error_pages/503.html\nErrorDocument 504 /src/assets/error_pages/504.html\n\n<VirtualHost|" $apache2Path/opencapture.conf
+#sed -i "s|<VirtualHost|ErrorDocument 400 /src/assets/error_pages/400.html\nErrorDocument 401 /src/assets/error_pages/401.html\nErrorDocument 403 /src/assets/error_pages/403.html\nErrorDocument 404 /src/assets/error_pages/404.html\nErrorDocument 500 /src/assets/error_pages/500.html\nErrorDocument 501 /src/assets/error_pages/501.html\nErrorDocument 502 /src/assets/error_pages/502.html\nErrorDocument 503 /src/assets/error_pages/503.html\nErrorDocument 504 /src/assets/error_pages/504.html\n\n<VirtualHost|" $apache2Path/opencapture.conf
 
 #####################
 # Update default referencial JSON file
 opencapturePath="/var/www/html/opencapture/"
 cd $opencapturePath
 
+#SECTIONS=$(crudini --get $opencapturePath/custom/custom.ini | sed 's/:.*//')
+#for custom_name in ${SECTIONS[@]}; do
+#    custom_name=${custom_name//[\.\-]/_}
+#    custom_name=$(echo "$custom_name" | tr "[:upper:]" "[:lower:]")
+#    cp $opencapturePath/instance/referencial/default_referencial_supplier_index.json.default $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier_index.json 2>/dev/null
+#    rm -f $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier.ods
+#    touch $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier.csv
+#done
+
+#####################
+# Update attachments docservers path
+docserverDefaultPath="/var/docservers/opencapture/"
+
 SECTIONS=$(crudini --get $opencapturePath/custom/custom.ini | sed 's/:.*//')
 for custom_name in ${SECTIONS[@]}; do
     custom_name=${custom_name//[\.\-]/_}
     custom_name=$(echo "$custom_name" | tr "[:upper:]" "[:lower:]")
-    cp $opencapturePath/instance/referencial/default_referencial_supplier_index.json.default $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier_index.json 2>/dev/null
-    rm -f $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier.ods
-    touch $opencapturePath/custom/$custom_name/instance/referencial/default_referencial_supplier.csv
+    psql_user=$(crudini --get $opencapturePath/custom/$custom_name/config/config.ini DATABASE postgresUser)
+    psql_password=$(crudini --get $opencapturePath/custom/$custom_name/config/config.ini DATABASE postgresPassword)
+    psql_database=$(crudini --get $opencapturePath/custom/$custom_name/config/config.ini DATABASE postgresDatabase)
+    psql_host=$(crudini --get $opencapturePath/custom/$custom_name/config/config.ini DATABASE postgresHost)
+    psql_port=$(crudini --get $opencapturePath/custom/$custom_name/config/config.ini DATABASE postgresPort)
+
+    export PGPASSWORD=$psql_password && psql -U $psql_user -d $psql_database -h $psql_host -p $psql_port -c \
+     "UPDATE docservers SET path=REPLACE(path, '$docserverDefaultPath' , '$docserverDefaultPath/$custom_name/')
+     WHERE docserver_id IN ('VERIFIER_ATTACHMENTS', 'SPLITTER_ATTACHMENTS') "
+
+    export PGPASSWORD=$psql_password && psql -U $psql_user -d $psql_database -h $psql_host -p $psql_port -c \
+     "UPDATE docservers SET path=REPLACE(path, '//' , '/')"
+
+    unset PGPASSWORD
 done
 
 #####################
--- install/migration_sql/3.3.0.sql
+++ install/migration_sql/3.3.0.sql
@@ -155,3 +155,13 @@ INSERT INTO "status" ("id", "label", "label_long", "module") VALUES ('WAIT_THIRD
 UPDATE workflows SET process = process || '{"allow_third_party_validation": false}' WHERE module = 'verifier';
 
 ALTER TABLE accounts_supplier ADD COLUMN "default_accounting_plan" INTEGER;
+
+INSERT INTO "docservers" ("docserver_id", "description", "path") VALUES ('VERIFIER_ATTACHMENTS', '[VERIFIER] Chemin vers le dossier contenant les pièces jointes', '/var/docservers/opencapture/verifier/attachments/');
+INSERT INTO "docservers" ("docserver_id", "description", "path") VALUES ('SPLITTER_ATTACHMENTS', '[SPLITTER] Chemin pour le stockage des pièces jointes', '/var/docservers/opencapture/splitter/attachments/');
+
+ALTER TABLE mailcollect ADD COLUMN secret VARCHAR;
+ALTER TABLE mailcollect ADD COLUMN tenant_id VARCHAR;
+ALTER TABLE mailcollect ADD COLUMN client_id VARCHAR;
+ALTER TABLE mailcollect ADD COLUMN oauth BOOLEAN DEFAULT FALSE;
+ALTER TABLE mailcollect ADD COLUMN scopes VARCHAR DEFAULT 'https://outlook.office.com/.default';
+ALTER TABLE mailcollect ADD COLUMN authority VARCHAR DEFAULT 'https://login.microsoftonline.com/';
