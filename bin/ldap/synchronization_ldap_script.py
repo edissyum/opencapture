6c63b5eb1c5d4f86bf1c70aa327421b5d7d4936c
Nathan Cheval <nathan.cheval@outlook.fr>
2024-10-07 16:22:49 +0200

    Improve LDAP synchro

--- bin/ldap/synchronization_ldap_script.py
+++ bin/ldap/synchronization_ldap_script.py
@@ -202,8 +202,8 @@ def check_database_users(ldap_users_data, default_role):
                             database.update({
                                 'table': ['users'],
                                 'set': {
-                                    'enabled': True,
-                                    'status': 'OK'
+                                    'status': 'OK',
+                                    'enabled': True
                                 },
                                 'where': ['username = %s'],
                                 'data': [oc_user[0]]
