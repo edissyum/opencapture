d7bf4185da6437f0d49d802e3c9a8299d92aad97
Nathan Cheval <nathan.cheval@outlook.fr>
2024-10-15 09:41:16 +0200

    Improve date REGEX

--- install/migration_sql/3.3.3.sql
+++ install/migration_sql/3.3.3.sql
@@ -1,4 +1,6 @@
 ALTER TABLE "splitter_batches" ADD COLUMN subject VARCHAR(255);
 INSERT INTO "regex" ("regex_id", "lang", "label", "content") VALUES ('currency', 'global', 'Devise', '((R\$|EUR|USD|CAD|AUD|A\$|CNY|CHF)|[£$€¥])\s*(\d+(?:[\.|,]\d{2})?)|(\d+(?:[\.|,]\d{2})?)\s*((R\$|EUR|USD|CAD|AUD|A\$|CNY|CHF)|[£$€¥])|((CURRENCY|TOTAL)\s*[,.:;(]?\s*(R\$|EUR|USD|CAD|AUD|A\$|CNY|CHF)[)]?)');
 
+UPDATE "regex" SET content = '([JFMASONDjfmasond][a-zA-Z_À-ÿ\.,-]{2,9})\s*(3[01]|[12][0-9]|0?[1-9][\.,-]?)[\.,-]?\s*((1|2|3){1}\d{1,3}|(1|2|3))|(((1[0-2]|0?[1-9])|\d{1}\w{2})\s?([JFMASONDjfmasond][a-zA-Z_À-ÿ\.,-]{2,9}|[\/,-\.](3[01]|[12][0-9]|0?[1-9])[\/,-\.])\s?((1|2|3){1}\d{1,3}|(1|2|3)))' WHERE regex_id = 'date' AND lang = 'eng';
+
 ALTER TABLE accounts_supplier ADD COLUMN "default_currency" VARCHAR(10);
