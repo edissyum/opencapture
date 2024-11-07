0d54372d98ac36e06ef32349df91ff0397cd3f77
Nathan Cheval <nathan.cheval@outlook.fr>
2024-11-07 16:57:03 +0100

    Improve french due date regex

--- install/migration_sql/3.3.3.sql
+++ install/migration_sql/3.3.3.sql
@@ -3,11 +3,13 @@ INSERT INTO "regex" ("regex_id", "lang", "label", "content") VALUES ('currency',
 
 UPDATE "regex" SET content = '([JFMASONDjfmasond][a-zA-Z_À-ÿ\.,-]{2,9})\s*(3[01]|[12][0-9]|0?[1-9][\.,-]?)[\.,-]?\s*((1|2|3){1}\d{1,3}|(1|2|3))|(((3[01]|[12][0-9]|0?[1-9])[\.,-]?)\s?([JFMASONDjfmasond][a-zA-Z_À-ÿ\.,-]{2,9}|[\/,-\.](3[01]|[12][0-9]|0?[1-9])[\/,-\.])\s?((1|2|3){1}\d{1,3}|(1|2|3)))' WHERE regex_id = 'date' AND lang = 'eng';
 
-UPDATE "regex" SET content = '(INVOICE\s*(NUMBER|#|NO|N(°)?)\s*(\.)?\s*(:)?).*' WHERE regex_id = 'invoice_number' AND lang='eng';
-UPDATE "regex" SET content = '(DELIVERY\s*(NOTE)?\s*(NUMBER|#|NO|N(°)?)\s*(\.)?\s*(:)?).*' WHERE regex_id = 'delivery_number' AND lang='eng';
-UPDATE "regex" SET content = '(((QUOT(E|ATION)|ORDER|PURCHASE\s*ORDER|P(/)?O)\s*(NUMBER|#|NO|N(°)?)|(SALES)\s*(ORDER)\s*(NUMBER|#|NO|N(°)?))\s*(\.)?\s*(:)?).*' WHERE regex_id = 'quotation_number' AND lang='eng';
-UPDATE "regex" SET content = '(TOTAL|GROSS)\s*(AMOUNT|DUE)(\s*PAID)?\s*(:)?\s*(\$|£|€|EUROS|EUR|CAD|USD)?\s*.*' WHERE regex_id = 'all_rates' AND lang='eng';
-UPDATE "regex" SET content = '(NET)\s*(AMOUNT|DUE)(\s*PAID)?\s*(:)?\s*(\$|£|€|EUROS|EUR|CAD|USD)?\s*.*' WHERE regex_id = 'no_rates' AND lang='eng';
-UPDATE "regex" SET content = '(VAT\s*(NUMBER|AMOUNT\s*)|TOTAL\s*TAXES)(\$|£|€|EUROS|EUR|CAD|USD)?\s*.*' WHERE regex_id = 'vat_amount' AND lang='eng';
+UPDATE "regex" SET content = '(INVOICE\s*(NUMBER|#|NO|N(°)?)\s*(\.)?\s*(:)?).*' WHERE regex_id = 'invoice_number' AND lang = 'eng';
+UPDATE "regex" SET content = '(DELIVERY\s*(NOTE)?\s*(NUMBER|#|NO|N(°)?)\s*(\.)?\s*(:)?).*' WHERE regex_id = 'delivery_number' AND lang = 'eng';
+UPDATE "regex" SET content = '(((QUOT(E|ATION)|ORDER|PURCHASE\s*ORDER|P(/)?O)\s*(NUMBER|#|NO|N(°)?)|(SALES)\s*(ORDER)\s*(NUMBER|#|NO|N(°)?))\s*(\.)?\s*(:)?).*' WHERE regex_id = 'quotation_number' AND lang = 'eng';
+UPDATE "regex" SET content = '(TOTAL|GROSS)\s*(AMOUNT|DUE)(\s*PAID)?\s*(:)?\s*(\$|£|€|EUROS|EUR|CAD|USD)?\s*.*' WHERE regex_id = 'all_rates' AND lang = 'eng';
+UPDATE "regex" SET content = '(NET)\s*(AMOUNT|DUE)(\s*PAID)?\s*(:)?\s*(\$|£|€|EUROS|EUR|CAD|USD)?\s*.*' WHERE regex_id = 'no_rates' AND lang = 'eng';
+UPDATE "regex" SET content = '(VAT\s*(NUMBER|AMOUNT\s*)|TOTAL\s*TAXES)(\$|£|€|EUROS|EUR|CAD|USD)?\s*.*' WHERE regex_id = 'vat_amount' AND lang = 'eng';
 
+UPDATE "regex" SET content = '((DATE)?\s*(D(''|\s*))?(E|É)CH(É|E)ANCE(\(S\))?\s*:?\s*?\s*(AU)?\s*|FACTURE\s*(A|À)\s*PAYER\s*AVANT\s*LE\s*(:)?\s*)' WHERE regex_id = 'due_date' AND lang = 'fra';
+    
 ALTER TABLE accounts_supplier ADD COLUMN "default_currency" VARCHAR(10);
