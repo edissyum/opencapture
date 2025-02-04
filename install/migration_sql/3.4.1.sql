cbb489794d95c58cf5735edbb9af3d49eee9fd73
Nathan Cheval <nathan.cheval@outlook.fr>
2025-02-04 10:01:36 +0100

    Add missing privileges

--- install/migration_sql/3.4.1.sql
+++ install/migration_sql/3.4.1.sql
@@ -1,1 +1,6 @@
-INSERT INTO "configurations" ("label", "data") VALUES ('enableAttachments', '{"type": "bool", "value": true, "description": "Activer l''affichage des pièces jointes dans les module Verifier et Splitter"}');
+INSERT INTO "configurations" ("label", "data") VALUES ('enableAttachments', '{"type": "bool", "value": false, "description": "Activer l''affichage des pièces jointes dans les module Verifier et Splitter"}');
+
+INSERT INTO "privileges" ("label", "parent") VALUES ('attachments_list_splitter', 'splitter');
+INSERT INTO "privileges" ("label", "parent") VALUES ('attachments_list_verifier', 'verifier');
+INSERT INTO "privileges" ("label", "parent") VALUES ('upload_attachments_verifier', 'verifier');
+INSERT INTO "privileges" ("label", "parent") VALUES ('upload_attachments_splitter', 'splitter');
