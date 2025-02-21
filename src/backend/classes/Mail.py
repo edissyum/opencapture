ff7f6b972d113139ee085890a1834ed0f3b78269
Nathan Cheval <nathan.cheval@outlook.fr>
2025-02-21 11:19:14 +0100

    Improve MailCollect

--- src/backend/classes/Mail.py
+++ src/backend/classes/Mail.py
@@ -200,7 +200,7 @@ class Mail:
             else:
                 data['attachments'].append({
                     'file': path,
-                    'format': 'pdf',
+                    'format': attachment['format'],
                     'filename': sanitize_filename(attachment['filename']) + attachment['format']
                 })
 
