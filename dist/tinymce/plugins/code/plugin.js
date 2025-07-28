79965dffba8f07dd4645983d84c56bf872cb6193
Nathan Cheval <nathan.cheval@outlook.fr>
2025-07-28 17:51:34 +0200

    Build prod

--- dist/tinymce/plugins/code/plugin.js
+++ dist/tinymce/plugins/code/plugin.js
@@ -1,5 +1,5 @@
 /**
- * TinyMCE version 7.9.1 (2025-05-29)
+ * TinyMCE version 8.0.1 (2025-07-28)
  */
 
 (function () {
@@ -32,7 +32,8 @@
                 items: [
                     {
                         type: 'textarea',
-                        name: 'code'
+                        name: 'code',
+                        spellcheck: false,
                     }
                 ]
             },
